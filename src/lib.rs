9bdefaa9a8e82d4581aa375575f3f08787a50fad
JC <8765278+Crzyrndm@users.noreply.github.com>
2022-07-30 13:08:12 +1200

    CRC verification and testing of CRC calculation

--- src/lib.rs
+++ src/lib.rs
@@ -45,6 +45,16 @@ pub fn calculate_crc16(bytes: &[u8]) -> u16 {
     crc16::State::<crc16::MODBUS>::calculate(bytes)
 }
 
+/// returns true if `bytes.len() > 4` and the last two bytes are the CRC16 of the preceding bytes
+pub fn verify_crc16(bytes: &[u8]) -> bool {
+    if bytes.len() < 4 {
+        false
+    } else {
+        let frame = Frame::new(bytes);
+        frame.calculate_crc().to_le_bytes() == frame.crc_bytes()
+    }
+}
+
 pub trait PacketLen {
     fn packet_len(&self) -> u8;
     fn minimum_len() -> u8;
@@ -64,15 +74,17 @@ impl<T: FixedLen> PacketLen for T {
     }
 }
 
-#[derive(PartialEq, Debug, Clone)]
+#[derive(PartialEq, Debug, Clone, Copy)]
 #[non_exhaustive] // new errors may be added later
 pub enum Error {
-    None,
+    /// Valid message lengths are 4-256 bytes
     InvalidLength,
-    InvalidCorrupt,
-    InvalidEncoding,
-    OtherAddress,
-    WrongFunction,
+    /// CRC verification failed
+    InvalidCrc,
+    /// Decoding failed because the function code was unknown
+    UnknownFunction,
+    /// message size is invalid for the function code
+    DecodeInvalidLength,
 }
 
 // std::error::Error trait obviously isn't available in no_std
@@ -80,5 +92,17 @@ pub enum Error {
 
 #[cfg(test)]
 mod tests {
-    //
+    use crate::{calculate_crc16, verify_crc16};
+
+    #[test]
+    fn crc_calculation() {
+        // example request message bytes from https://www.simplymodbus.ca/FC06.htm
+        let message = [0x11_u8, 0x06, 0x00, 0x01, 0x00, 0x03, 0x9A, 0x9B];
+        let crc_idx = message.len() - 2;
+        assert_eq!(
+            calculate_crc16(&message[..crc_idx]),
+            u16::from_le_bytes([0x9A, 0x9B]) // CRC is little endian
+        );
+        assert!(verify_crc16(&message));
+    }
 }
